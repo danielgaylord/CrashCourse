578543b2fdb7fd946caea51f2be00c67cd22fec5
Daniel Gaylord <danielgaylord@gmail.com>
2016-05-26 14:04:09 -0400

    Started Chater 4

---
+++ Chapter 4/animals.py
@@ -0,0 +1,6 @@
+animals = ['dolphin', 'jaguar', 'python']
+
+for animal in animals:
+    print('A ' + animal + ' is not just an animal...')
+
+print('\nThey are also game systems or programming languages!')
---
+++ Chapter 4/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['hawaiian', 'meat lovers', 'the works']
+
+for pizza in pizzas:
+    print("I genuinely like " + pizza + " pizza")
+
+print("\nPizza is good.\nPizza is great.\nIf I eat too much,\nI gain lots of weight!")
