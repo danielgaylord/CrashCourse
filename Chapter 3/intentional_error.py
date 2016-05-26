878620231cb4b167530b27f1509c3d2d7f088c7b
Daniel Gaylord <danielgaylord@gmail.com>
2016-05-26 13:51:11 -0400

    Finished Chapter 3

---
+++ Chapter 3/intentional_error.py
@@ -0,0 +1,10 @@
+things = ['fire', 'water', 'wind', 'earth', 'heart']
+
+#correct array access
+print(things[-1])
+print(things[0])
+print(things[4])
+
+#incorrect array access
+#print(things[5])
+#print(things[-6])
