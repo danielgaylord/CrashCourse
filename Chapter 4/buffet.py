e4d789831010854933bd4d04d9c0b873cfccc107
Daniel Gaylord <danielgaylord@gmail.com>
2016-05-30 17:39:27 -0400

    Continued Chapter 4

---
+++ Chapter 4/buffet.py
@@ -0,0 +1,11 @@
+buffet = ('chicken nuggets', 'fries', 'burgers', 'salad', 'soup')
+
+print("Old menu:")
+for item in buffet:
+    print(item)
+
+buffet = ('chicken', 'fries', 'steak', 'salad', 'soup')
+
+print("\nNew menu:")
+for item in buffet:
+    print(item)
