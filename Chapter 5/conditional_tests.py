7ce1526b0ee415dd9ae80313c011e48aae66ea53
Daniel Gaylord <danielgaylord@gmail.com>
2016-06-07 14:45:27 -0400

    Started Chapter 5

---
+++ Chapter 5/conditional_tests.py
@@ -0,0 +1,56 @@
+name = 'Dan'
+
+print('Is name == "Dan"? I think it is!')
+print(name == "Dan")
+
+print('Is name != "Dan"? No, no, no!')
+print(name != "Dan")
+
+print('Is name == "dan"? Close, but no cigar!')
+print(name == "dan")
+
+print('Is name.lower() == "dan"? Yeah!')
+print(name.lower() == "dan")
+
+print('Is name.upper() == "DAN"? You betcha!')
+print(name.upper() == "DAN")
+
+print('Is name == "Joe"? Are you crazy?!')
+print(name == "Joe")
+
+print('Is name.capitalize() == "DAN"? Nah!')
+print(name.capitalize() == "DAN")
+
+print('Is name < "Zach"? It should be!')
+print(name < "Zach")
+
+print('Is name > "Adam"? Why, yes!')
+print(name > "Adam")
+
+print('Is name.swapcase() == "Dan"? Not a chance!')
+print(name.swapcase() == "Dan")
+
+value = 10
+
+print('Is value == "10"? Nope!')
+print(value == "10")
+
+print('Is value <= 7? No!')
+print(value <= 7)
+
+print('Is value > 7? Yes!')
+print(value > 7)
+
+print('Is value > 5 and value < 15? Yes!')
+print(value > 5 and value < 15)
+
+print('Is value < 3 or value > 20? No!')
+print(value < 3 or value > 20)
+
+list = ['red', 'blue', 'green']
+
+print('Is "yellow" in list? No!')
+print('yellow' in list)
+
+print('Is "yellow" not in list? Yes!')
+print('yellow' not in list)
